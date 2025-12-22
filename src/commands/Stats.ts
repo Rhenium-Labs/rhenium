5d556ef5a0c66aede8706e9548515475a7207766
cobaltorum <216449108+cobaltorum@users.noreply.github.com>
2025-12-22 12:37:38 +0100

    feat(commands): Add a stats command.

---
+++ src/commands/Stats.ts
@@ -0,0 +1,97 @@
+import { EmbedBuilder, Message } from "discord.js";
+import { Command } from "#classes/Command.js";
+
+import type { MessageReplyData } from "#utils/Types.js";
+
+import ms from "ms";
+import { prisma } from "#root/index.js";
+import { DEVELOPER_IDS } from "#utils/Constants.js";
+
+export default class Stats extends Command {
+	constructor() {
+		super({
+			name: "stats",
+			aliases: ["proc", "process"],
+			description: "Get information about the current process."
+		});
+	}
+
+	public override async messageRun(message: Message<true>): Promise<MessageReplyData> {
+		if (!DEVELOPER_IDS.includes(message.author.id)) {
+			return {
+				error: "You do not have permission to use this command."
+			};
+		}
+
+		// Process Uptime.
+		const processUptime = Math.round(Math.floor(process.uptime() * 1000));
+		const processUptimeStr = ms(processUptime, { long: true });
+
+		// Client Uptime.
+		const clientUptime = Math.round(Math.floor(this.client.uptime));
+		const clientUptimeStr = ms(clientUptime, { long: true });
+
+		// Memory Usage.
+		const memoryUsage = process.memoryUsage();
+		const rss = (memoryUsage.rss / 1024 / 1024).toFixed(0);
+		const heapUsed = (memoryUsage.heapUsed / 1024 / 1024).toFixed(0);
+		const heapTotal = (memoryUsage.heapTotal / 1024 / 1024).toFixed(0);
+
+		// Cache.
+		const { guilds, users, channels } = this.client;
+
+		// Database Size.
+		const dbSizeQuery = await prisma.$queryRaw<DatabaseSizeResult[]>`
+                SELECT pg_database_size(current_database()) / (1024 * 1024) as size_in_mb
+                FROM pg_database
+                WHERE datname = current_database()
+                LIMIT 1
+            `;
+
+		const dbSize = dbSizeQuery[0].size_in_mb;
+
+		const embed = new EmbedBuilder()
+			.setColor("NotQuiteBlack")
+			.setAuthor({ name: this.client.user.username, iconURL: this.client.user.displayAvatarURL() })
+			.setFields([
+				{
+					name: "Heartbeat",
+					value: `${this.client.ws.ping}ms`,
+					inline: true
+				},
+				{
+					name: "Client Uptime",
+					value: clientUptimeStr,
+					inline: true
+				},
+				{
+					name: "Process Uptime",
+					value: processUptimeStr,
+					inline: true
+				},
+				{
+					name: "Memory Usage",
+					value: `${heapUsed} MB / ${heapTotal} MB / ${rss} MB`,
+					inline: true
+				},
+				{
+					name: "Cache Entries",
+					value: `${users.cache.size} Users / ${guilds.cache.size} Guilds / ${channels.cache.size} Channels`,
+					inline: true
+				},
+				{
+					name: "Database Size",
+					value: `${dbSize} MB`,
+					inline: true
+				}
+			])
+			.setFooter({ text: `Client ID: ${this.client.user.id}` })
+			.setTimestamp();
+
+		return { embeds: [embed] };
+	}
+}
+
+interface DatabaseSizeResult {
+	size_in_mb: number;
+}
