3664e70113f213bac9c9c9e620aa8b93592e1496
cobaltorum <216449108+cobaltorum@users.noreply.github.com>
2026-01-31 16:50:05 +0100

    chore: Remove deprecated sentry option.

--- src/index.ts
+++ src/index.ts
@@ -90,7 +90,6 @@ async function main(): Promise<void> {
 		dsn: process.env.SENTRY_DSN,
 		environment: process.env.NODE_ENV || "production",
 		tracesSampleRate: 1.0,
-		profilesSampleRate: 1.0,
 		sampleRate: 1.0,
 		enableLogs: true,
 		integrations: [
