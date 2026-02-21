f567738e52453b9a0525c90e9074b1f1df5093de
cobaltorum <216449108+cobaltorum@users.noreply.github.com>
2026-02-21 22:26:17 +0100

    chore: Update tsconfig.

--- tests/env.test.ts
+++ tests/env.test.ts
@@ -1,6 +1,6 @@
 import { fromError } from "zod-validation-error";
 import { envZodSchema } from "../src/env";
-import { describe, test } from "node:test";
+import { describe, test } from "bun:test";
 
 describe("Environment Variables Validation", () => {
 	test(".env", async () => {
