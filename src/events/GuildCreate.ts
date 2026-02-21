3fbfbb18182991e4de122ca50233c0317e37b6cc
cobaltorum <216449108+cobaltorum@users.noreply.github.com>
2026-02-22 00:51:03 +0100

    refactor(events): Prevent the bot from joining unwhitelisted guilds.

--- src/events/GuildCreate.ts
+++ src/events/GuildCreate.ts
@@ -1,14 +1,26 @@
 import { Events, type Guild } from "discord.js";
+import { getWhitelistStatus } from "@utils/index";
 
 import ConfigManager from "@config/ConfigManager";
 import EventListener from "@events/EventListener";
+import Logger from "@utils/Logger";
 
 export default class GuildCreate extends EventListener {
 	constructor() {
 		super(Events.GuildCreate);
 	}
 
-	execute(guild: Guild): void {
+	async execute(guild: Guild): Promise<void> {
+		const whitelisted = await getWhitelistStatus(guild.id);
+
+		if (!whitelisted) {
+			Logger.warn(`Guild "${guild.name}" (${guild.id}) is not whitelisted. Leaving...`);
+			return guild
+				.leave()
+				.then(() => Logger.info(`Left guild "${guild.name}" (${guild.id}).`))
+				.catch(() => {});
+		}
+
 		// Trigger the creation/loading of the guild config.
 		ConfigManager.get(guild.id);
 	}
